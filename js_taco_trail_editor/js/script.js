e82ccc0263624c96297e4f8c999a72f50592724e
Philipp Reuter <reuter.philipp@ymail.com>
2021-10-19 03:39:05 +0200

    Better List Drag and Drop, still slow!

--- js_taco_trail_editor/js/script.js
+++ js_taco_trail_editor/js/script.js
@@ -93,27 +93,11 @@ function unpack_trail(trail_file) {
 
         // Loop to unpack postionBuffer
         var postionBuffer = arrayBuffer.slice(offset, arrayBuffer.length);
-        var index = 0
+
         for (i = 0; i < postionBuffer.byteLength; i += 12) {
-            position_json_data = {
-                "index": index++,
-                "x": null,
-                "y": null,
-                "z": null
-            };
-
-            // if (i === 0) {
-            //     console.log(postionBuffer.slice(i, i + 4))
-            // }
-
-            var x_f32 = new Float32Array(postionBuffer.slice(i, i + 4));
-            var x_f32value = x_f32[0];
-
-            var y_f32 = new Float32Array(postionBuffer.slice(i + 4, i + 8));
-            var y_f32value = y_f32[0];
-
-            var z_f32 = new Float32Array(postionBuffer.slice(i + 8, i + 12));
-            var z_f32value = z_f32[0];
+            var x_f32value = float32BytesToFouble(postionBuffer.slice(i, i + 4));
+            var y_f32value = float32BytesToFouble(postionBuffer.slice(i + 4, i + 8));
+            var z_f32value = float32BytesToFouble(postionBuffer.slice(i + 8, i + 12));
 
             addPosition(parseInt(i / 12), x_f32value, y_f32value, z_f32value);
         }
@@ -130,19 +114,6 @@ function addPosition(i, x, y, z) {
     document.getElementById("addRow").style.visibility = 'visible'
 
     table = document.getElementById("table");
-
-    // Query all rows
-    table.querySelectorAll('tr').forEach(function(row, index) {
-        // Ignore the header
-        // We don't want user to change the order of header
-        if (index === 0) {
-            return;
-        }
-
-        const firstCell = row.firstElementChild;
-        firstCell.addEventListener('mousedown', mouseDownHandler);
-    });
-
 };
 
 function saveAs(blob, fileName) {
@@ -244,6 +215,10 @@ function positionToHtml(i, x, y, z) {
     tableRow0.appendChild(tableData3);
     tableRow0.appendChild(tableData5);
 
+    tableRow0.draggable = true;
+    tableRow0.setAttribute("ondragstart", "dragStart(event)");
+    tableRow0.setAttribute("ondragover", "dragOver(event)");
+
     return tableRow0;
 }
 
@@ -298,6 +273,12 @@ function doubleToFloat32Bytes(number) {
     return bytes;
 }
 
+function float32BytesToFouble(ArrayBuffer) {
+    var y_f32 = new Float32Array(ArrayBuffer);
+
+    return y_f32[0];
+}
+
 function saveAs(blob, fileName) {
     var url = window.URL.createObjectURL(blob);
 
@@ -318,181 +299,18 @@ function saveAs(blob, fileName) {
     }, 1000);
 }
 
-var table = null;
-let draggingEle;
-let draggingRowIndex;
-let placeholder;
-let list;
-let isDraggingStarted = false;
+var row;
 
-// The current position of mouse relative to the dragging element
-let x = 0;
-let y = 0;
+function dragStart(e) {
+    row = e.target;
+}
 
-// Swap two nodes
-const swap = function(nodeA, nodeB) {
-    const parentA = nodeA.parentNode;
-    const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+function dragOver(e) {
+    e.preventDefault();
 
-    // Move `nodeA` to before the `nodeB`
-    nodeB.parentNode.insertBefore(nodeA, nodeB);
-
-    // Move `nodeB` to before the sibling of `nodeA`
-    parentA.insertBefore(nodeB, siblingA);
-};
-
-// Check if `nodeA` is above `nodeB`
-const isAbove = function(nodeA, nodeB) {
-    // Get the bounding rectangle of nodes
-    const rectA = nodeA.getBoundingClientRect();
-    const rectB = nodeB.getBoundingClientRect();
-
-    return rectA.top + rectA.height / 2 < rectB.top + rectB.height / 2;
-};
-
-const cloneTable = function() {
-    const rect = table.getBoundingClientRect();
-    const width = parseInt(window.getComputedStyle(table).width);
-
-    list = document.createElement('div');
-    list.classList.add('clone-list');
-    list.style.position = 'absolute';
-    list.style.left = `${rect.left}px`;
-    list.style.top = `${rect.top}px`;
-    table.parentNode.insertBefore(list, table);
-
-    // Hide the original table
-    table.style.visibility = 'hidden';
-
-    table.querySelectorAll('tr').forEach(function(row) {
-        // Create a new table from given row
-        const item = document.createElement('div');
-        item.classList.add('draggable');
-
-        const newTable = document.createElement('table');
-        newTable.setAttribute('class', 'clone-table');
-        newTable.style.width = `${width}px`;
-
-        const newRow = document.createElement('tr');
-        const cells = [].slice.call(row.children);
-        cells.forEach(function(cell) {
-            const newCell = cell.cloneNode(true);
-            newCell.style.width = `${parseInt(window.getComputedStyle(cell).width)}px`;
-            newRow.appendChild(newCell);
-        });
-
-        newTable.appendChild(newRow);
-        item.appendChild(newTable);
-        list.appendChild(item);
-    });
-};
-
-const mouseDownHandler = function(e) {
-    e.preventDefault()
-        // Get the original row
-    const originalRow = e.target.parentNode.parentNode;
-    draggingRowIndex = [].slice.call(table.querySelectorAll('tr')).indexOf(originalRow);
-
-    // Determine the mouse position
-    x = e.clientX;
-    y = e.clientY;
-
-    // Attach the listeners to `document`
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-};
-
-const mouseMoveHandler = function(e) {
-    e.preventDefault()
-    if (!isDraggingStarted) {
-        isDraggingStarted = true;
-
-        cloneTable();
-
-        draggingEle = [].slice.call(list.children)[draggingRowIndex];
-        draggingEle.classList.add('dragging');
-
-        // Let the placeholder take the height of dragging element
-        // So the next element won't move up
-        placeholder = document.createElement('div');
-        placeholder.classList.add('placeholder');
-        draggingEle.parentNode.insertBefore(placeholder, draggingEle.nextSibling);
-        placeholder.style.height = `${draggingEle.offsetHeight}px`;
-    }
-
-    // Set position for dragging element
-    draggingEle.style.position = 'absolute';
-    draggingEle.style.top = `${draggingEle.offsetTop + e.clientY - y}px`;
-    draggingEle.style.left = `${draggingEle.offsetLeft + e.clientX - x}px`;
-
-    // Reassign the position of mouse
-    x = e.clientX;
-    y = e.clientY;
-
-    // The current order
-    // prevEle
-    // draggingEle
-    // placeholder
-    // nextEle
-    const prevEle = draggingEle.previousElementSibling;
-    const nextEle = placeholder.nextElementSibling;
-
-    // The dragging element is above the previous element
-    // User moves the dragging element to the top
-    // We don't allow to drop above the header
-    // (which doesn't have `previousElementSibling`)
-    if (prevEle && prevEle.previousElementSibling && isAbove(draggingEle, prevEle)) {
-        // The current order    -> The new order
-        // prevEle              -> placeholder
-        // draggingEle          -> draggingEle
-        // placeholder          -> prevEle
-        swap(placeholder, draggingEle);
-        swap(placeholder, prevEle);
-        return;
-    }
-
-    // The dragging element is below the next element
-    // User moves the dragging element to the bottom
-    if (nextEle && isAbove(nextEle, draggingEle)) {
-        // The current order    -> The new order
-        // draggingEle          -> nextEle
-        // placeholder          -> placeholder
-        // nextEle              -> draggingEle
-        swap(nextEle, placeholder);
-        swap(nextEle, draggingEle);
-    }
-};
-
-const mouseUpHandler = function() {
-    // Remove the placeholder
-    placeholder && placeholder.parentNode.removeChild(placeholder);
-
-    draggingEle.classList.remove('dragging');
-    draggingEle.style.removeProperty('top');
-    draggingEle.style.removeProperty('left');
-    draggingEle.style.removeProperty('position');
-
-    // Get the end index
-    const endRowIndex = [].slice.call(list.children).indexOf(draggingEle);
-
-    isDraggingStarted = false;
-
-    // Remove the `list` element
-    list.parentNode.removeChild(list);
-
-    // Move the dragged row to `endRowIndex`
-    let rows = [].slice.call(table.querySelectorAll('tr'));
-    draggingRowIndex > endRowIndex ?
-        rows[endRowIndex].parentNode.insertBefore(rows[draggingRowIndex], rows[endRowIndex]) :
-        rows[endRowIndex].parentNode.insertBefore(
-            rows[draggingRowIndex],
-            rows[endRowIndex].nextSibling
-        );
-
-    // Bring back the table
-    table.style.removeProperty('visibility');
-
-    // Remove the handlers of `mousemove` and `mouseup`
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
-};
+    let children = Array.from(e.target.parentNode.parentNode.children);
+    if (children.indexOf(e.target.parentNode) > children.indexOf(row))
+        e.target.parentNode.after(row);
+    else
+        e.target.parentNode.before(row);
+}
