947c4226d54d429af6a1231e1933b0f7abeaa065
Philipp Reuter <reuter.philipp@ymail.com>
2022-08-21 22:41:42 +0200

    Bug fixed with Map ID.

--- js_taco_trail_editor/static/taco/js/script.js
+++ js_taco_trail_editor/static/taco/js/script.js
@@ -12,6 +12,11 @@ const changeAllY = document.getElementById("changeAllY")
 const changeAllZ = document.getElementById("changeAllZ")
 const changeAllValue = document.getElementById("changeAllValue")
 const mainContend = document.getElementById("mainContend")
+const indexStart = document.getElementById("indexStart")
+const indexEnd = document.getElementById("indexEnd")
+const checkboxIndex = document.getElementById("checkboxIndex")
+const checkboxFileName = document.getElementById("checkboxFileName")
+const fileName = document.getElementById("newFileName")
 
 inputElement.addEventListener("change", display_trail, false);
 mapIdE.addEventListener("change", show_map_name, false);
@@ -301,9 +306,13 @@ function saveTrail() {
     });
 
     var file_blob = new Blob(fileBytes)
-    var fileName = document.getElementById("newFileName").value
-    saveAs(file_blob, fileName);
-}
+    if (checkboxFileName.checked) {
+        saveAs(file_blob, fileName.value);
+    } else {
+        saveAs(file_blob, inputElement.value.replace(/.*[\/\\]/, ''));
+    }
+    
+};
 
 function Int32toBytes(num) {
     var buffer = new ArrayBuffer(4); // an Int32 takes 4 bytes
@@ -311,7 +320,7 @@ function Int32toBytes(num) {
     var bytes = new Uint8Array(buffer).reverse();
 
     return bytes;
-}
+};
 
 function doubleToByteArray(number) {
     var buffer = new ArrayBuffer(4);
@@ -319,7 +328,7 @@ function doubleToByteArray(number) {
     var bytes = new Uint8Array(buffer).reverse(); // reverse to get little endian
 
     return bytes;
-}
+};
 
 function saveAs(blob, fileName) {
     var url = window.URL.createObjectURL(blob);
@@ -339,205 +348,86 @@ function saveAs(blob, fileName) {
     setTimeout(function() {
         window.URL.revokeObjectURL(url);
     }, 1000);
-}
+};
 
-function changeAll() {
-    if (changeAllX.checked) {
-        changeAxis("x")
+function changeValues() {
+    if (changeAllValue.value != "") {
+        if (changeAllX.checked) {
+            changeAxis("x", "=");
+        }
+
+        if (changeAllY.checked) {
+            changeAxis("y", "=");
+        }
+
+        if (changeAllZ.checked) {
+            changeAxis("z", "=");
+        }
+    }
+};
+
+function addValues() {
+    if (changeAllValue.value != "") {
+        if (changeAllX.checked) {
+            changeAxis("x", "+");
+        }
+
+        if (changeAllY.checked) {
+            changeAxis("y", "+");
+        }
+
+        if (changeAllZ.checked) {
+            changeAxis("z", "+");
+        }
+    }
+};
+
+function subValues() {
+    if (changeAllValue.value != "") {
+        if (changeAllX.checked) {
+            changeAxis("x", "-");
+        }
+
+        if (changeAllY.checked) {
+            changeAxis("y", "-");
+        }
+
+        if (changeAllZ.checked) {
+            changeAxis("z", "-");
+        }
+    }
+};
+
+function changeAxis(axis, opp) {
+    var tableArray = [].slice.call(table.children).slice(1);
+
+    if (checkboxIndex.checked && indexStart.value.length > 0 && indexEnd.value.length > 0) {
+        tableArray = tableArray.slice(parseFloat(indexStart.value), parseFloat(indexEnd.value) + 1);
     }
 
-    if (changeAllY.checked) {
-        changeAxis("y")
-    }
-
-    if (changeAllZ.checked) {
-        changeAxis("z")
-    }
-}
-
-function changeAxis(axis) {
-    let tableArray = table.children;
-    for (var i = 1; i < tableArray.length; i++) {
+    for (var i = 0; i < tableArray.length; i++) {
         let rowAxis = tableArray[i].querySelector('#' + axis).children[0];
-        rowAxis.value = changeAllValue.value;
+        switch (opp) {
+            case "=":
+                rowAxis.value = parseFloat(changeAllValue.value);
+                break;
+            case "+":
+                rowAxis.value = parseFloat(rowAxis.value) + parseFloat(changeAllValue.value);
+                break;
+            case "-":
+                rowAxis.value = parseFloat(rowAxis.value) - parseFloat(changeAllValue.value);
+                break;
+            default:
+                console.log("changeAxis undefined opperation");
+        }
+    }
+};
+
+function toggleFileName() {
+    console.log("Toggle")
+    if (checkboxFileName.checked) {
+        fileName.disabled = false;
+    } else {
+        fileName.disabled = true;
     }
 }
-
-// var table = null;
-// let draggingEle;
-// let draggingRowIndex;
-// let placeholder;
-// let list;
-// let isDraggingStarted = false;
-
-// // The current position of mouse relative to the dragging element
-// let x = 0;
-// let y = 0;
-
-// // Swap two nodes
-// const swap = function(nodeA, nodeB) {
-//     const parentA = nodeA.parentNode;
-//     const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
-
-//     // Move `nodeA` to before the `nodeB`
-//     nodeB.parentNode.insertBefore(nodeA, nodeB);
-
-//     // Move `nodeB` to before the sibling of `nodeA`
-//     parentA.insertBefore(nodeB, siblingA);
-// };
-
-// // Check if `nodeA` is above `nodeB`
-// const isAbove = function(nodeA, nodeB) {
-//     // Get the bounding rectangle of nodes
-//     const rectA = nodeA.getBoundingClientRect();
-//     const rectB = nodeB.getBoundingClientRect();
-
-//     return rectA.top + rectA.height / 2 < rectB.top + rectB.height / 2;
-// };
-
-// const cloneTable = function() {
-//     const rect = table.getBoundingClientRect();
-//     const width = parseInt(window.getComputedStyle(table).width);
-
-//     list = document.createElement('div');
-//     list.classList.add('clone-list');
-//     list.style.position = 'absolute';
-//     list.style.left = `${rect.left}px`;
-//     list.style.top = `${rect.top}px`;
-//     table.parentNode.insertBefore(list, table);
-
-//     // Hide the original table
-//     table.style.visibility = 'hidden';
-
-//     table.querySelectorAll('tr').forEach(function(row) {
-//         // Create a new table from given row
-//         const item = document.createElement('div');
-//         item.classList.add('draggable');
-
-//         const newTable = document.createElement('table');
-//         newTable.setAttribute('class', 'clone-table');
-//         newTable.style.width = `${width}px`;
-
-//         const newRow = document.createElement('tr');
-//         const cells = [].slice.call(row.children);
-//         cells.forEach(function(cell) {
-//             const newCell = cell.cloneNode(true);
-//             newCell.style.width = `${parseInt(window.getComputedStyle(cell).width)}px`;
-//             newRow.appendChild(newCell);
-//         });
-
-//         newTable.appendChild(newRow);
-//         item.appendChild(newTable);
-//         list.appendChild(item);
-//     });
-// };
-
-// const mouseDownHandler = function(e) {
-//     e.preventDefault()
-//         // Get the original row
-//     const originalRow = e.target.parentNode.parentNode;
-//     draggingRowIndex = [].slice.call(table.querySelectorAll('tr')).indexOf(originalRow);
-
-//     // Determine the mouse position
-//     x = e.clientX;
-//     y = e.clientY;
-
-//     // Attach the listeners to `document`
-//     document.addEventListener('mousemove', mouseMoveHandler);
-//     document.addEventListener('mouseup', mouseUpHandler);
-// };
-
-// const mouseMoveHandler = function(e) {
-//     e.preventDefault()
-//     if (!isDraggingStarted) {
-//         isDraggingStarted = true;
-
-//         cloneTable();
-
-//         draggingEle = [].slice.call(list.children)[draggingRowIndex];
-//         draggingEle.classList.add('dragging');
-
-//         // Let the placeholder take the height of dragging element
-//         // So the next element won't move up
-//         placeholder = document.createElement('div');
-//         placeholder.classList.add('placeholder');
-//         draggingEle.parentNode.insertBefore(placeholder, draggingEle.nextSibling);
-//         placeholder.style.height = `${draggingEle.offsetHeight}px`;
-//     }
-
-//     // Set position for dragging element
-//     draggingEle.style.position = 'absolute';
-//     draggingEle.style.top = `${draggingEle.offsetTop + e.clientY - y}px`;
-//     draggingEle.style.left = `${draggingEle.offsetLeft + e.clientX - x}px`;
-
-//     // Reassign the position of mouse
-//     x = e.clientX;
-//     y = e.clientY;
-
-//     // The current order
-//     // prevEle
-//     // draggingEle
-//     // placeholder
-//     // nextEle
-//     const prevEle = draggingEle.previousElementSibling;
-//     const nextEle = placeholder.nextElementSibling;
-
-//     // The dragging element is above the previous element
-//     // User moves the dragging element to the top
-//     // We don't allow to drop above the header
-//     // (which doesn't have `previousElementSibling`)
-//     if (prevEle && prevEle.previousElementSibling && isAbove(draggingEle, prevEle)) {
-//         // The current order    -> The new order
-//         // prevEle              -> placeholder
-//         // draggingEle          -> draggingEle
-//         // placeholder          -> prevEle
-//         swap(placeholder, draggingEle);
-//         swap(placeholder, prevEle);
-//         return;
-//     }
-
-//     // The dragging element is below the next element
-//     // User moves the dragging element to the bottom
-//     if (nextEle && isAbove(nextEle, draggingEle)) {
-//         // The current order    -> The new order
-//         // draggingEle          -> nextEle
-//         // placeholder          -> placeholder
-//         // nextEle              -> draggingEle
-//         swap(nextEle, placeholder);
-//         swap(nextEle, draggingEle);
-//     }
-// };
-
-// const mouseUpHandler = function() {
-//     // Remove the placeholder
-//     placeholder && placeholder.parentNode.removeChild(placeholder);
-
-//     draggingEle.classList.remove('dragging');
-//     draggingEle.style.removeProperty('top');
-//     draggingEle.style.removeProperty('left');
-//     draggingEle.style.removeProperty('position');
-
-//     // Get the end index
-//     const endRowIndex = [].slice.call(list.children).indexOf(draggingEle);
-
-//     isDraggingStarted = false;
-
-//     // Remove the `list` element
-//     list.parentNode.removeChild(list);
-
-//     // Move the dragged row to `endRowIndex`
-//     let rows = [].slice.call(table.querySelectorAll('tr'));
-//     draggingRowIndex > endRowIndex ?
-//         rows[endRowIndex].parentNode.insertBefore(rows[draggingRowIndex], rows[endRowIndex]) :
-//         rows[endRowIndex].parentNode.insertBefore(
-//             rows[draggingRowIndex],
-//             rows[endRowIndex].nextSibling
-//         );
-
-//     // Bring back the table
-//     table.style.removeProperty('visibility');
-
-//     // Remove the handlers of `mousemove` and `mouseup`
-//     document.removeEventListener('mousemove', mouseMoveHandler);
-//     document.removeEventListener('mouseup', mouseUpHandler);
-// };
